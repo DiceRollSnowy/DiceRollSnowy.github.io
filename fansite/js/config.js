6688593c42d2ffbc7e3de6de634881f6ec86e861
DiceRoll <diced.margarine.21@gmail.com>
2026-09-10 17:24:51 +0900

    ファンサーバ関連のファイルを削除、別リポジトリ（ページ）に移動、リンク先を更新

--- fansite/js/config.js
+++
@@ -1,2 +0,0 @@
-// 2026/09/09 - 30days
-const FAN_SERVER_URL = "https://discord.gg/w5YPBdcAw";
--- fansite/js/fan.js
+++
@@ -1,9 +0,0 @@
-// ファンサーバのリンクを反映
-document.addEventListener("DOMContentLoaded", () => {
-    const fanServerLink = document.getElementById("fanServerLink");
-
-    if (fanServerLink) 
-    {
-        fanServerLink.href = FAN_SERVER_URL;
-    }
-});
